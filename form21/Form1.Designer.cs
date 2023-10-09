b8ba75f0a6f7f8c0c6ee6a72096b8cb591f20ded
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-10-09 12:22:34 +0300

    Add files via upload

---
+++ form21/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace form21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.cbKirmizi = new System.Windows.Forms.CheckBox();
+            this.lblYaz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(91, 58);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(80, 52);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // cbKirmizi
+            // 
+            this.cbKirmizi.AutoSize = true;
+            this.cbKirmizi.Location = new System.Drawing.Point(91, 149);
+            this.cbKirmizi.Name = "cbKirmizi";
+            this.cbKirmizi.Size = new System.Drawing.Size(72, 17);
+            this.cbKirmizi.TabIndex = 2;
+            this.cbKirmizi.Text = "Kirmiziyap";
+            this.cbKirmizi.UseVisualStyleBackColor = true;
+            // 
+            // lblYaz
+            // 
+            this.lblYaz.AutoSize = true;
+            this.lblYaz.Location = new System.Drawing.Point(88, 210);
+            this.lblYaz.Name = "lblYaz";
+            this.lblYaz.Size = new System.Drawing.Size(19, 13);
+            this.lblYaz.TabIndex = 3;
+            this.lblYaz.Text = "***";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(271, 302);
+            this.Controls.Add(this.lblYaz);
+            this.Controls.Add(this.cbKirmizi);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.CheckBox cbKirmizi;
+        private System.Windows.Forms.Label lblYaz;
+    }
+}
+
---
+++ form21/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            if(cbKirmizi.Checked == true)
+            {
+                lblYaz.Text = "Kirmizi Yapıldı";
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                lblYaz.Text = "Kirmizi Kaldırıldı";
+                this.BackColor = Color.Red;
+            }
+        }
+    }
+}
